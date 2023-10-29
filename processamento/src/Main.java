532282ecbce7c3e1069341f9c5e058b9ffc4334d
Murilo Sopi <dev.murilosopi@gmail.com>
2023-10-29 09:16:35 -0300

    casting

---
+++ processamento/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		/* Casting */
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		
+		/* Casting */
+		resultado = (double) a / b;
+		
+		System.out.print(resultado);
+		
+		
+		double c = 2.0;
+		int d = (int) c;
+		
+	}
+
+}
